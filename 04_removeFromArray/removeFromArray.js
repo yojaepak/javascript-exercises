f65bc36892a1458429385d3fbd3133c56256a026
Young Park <yjp.youngpark@gmail.com>
2022-06-15 15:59:28 -0400

    splice out number from array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, removal) {
+    let i = 0;
+    for (const num of array) {
+   
+        if (num === removal) {
+            array.splice(i, 1);
+  
+        }
+        i++;
+    }
+    
+    return array;
 };
 
 // Do not edit below this line
