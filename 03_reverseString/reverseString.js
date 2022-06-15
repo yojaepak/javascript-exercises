0ef8d73f557732784b9ae10df6f2b14e2ee34aa9
Young Park <yjp.youngpark@gmail.com>
2022-06-15 15:33:49 -0400

    reverse string input

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let result = "";
+    for (const letter of string) {
+        result = letter + result;
+    }
+    return result;
 };
 
 // Do not edit below this line
