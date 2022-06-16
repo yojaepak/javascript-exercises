2435194dae8009e591c8920327142e264ec239a2
Young Park <yjp.youngpark@gmail.com>
2022-06-16 17:36:11 -0400

    Add sum of numbers between inputs

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,9 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    let result = 0;
+    for (let i = start; i < end + 1; i++) {
+        result += i;
+    }
+    return result;
 };
 
 // Do not edit below this line
