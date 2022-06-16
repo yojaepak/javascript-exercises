1047ffd9f8e0cc9ac95895460f3d1577a987c67f
Young Park <yjp.youngpark@gmail.com>
2022-06-16 18:17:42 -0400

    convert celcius to farenheit and vice versa.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(ftemp) {
+  let celcius = (ftemp - 32) * (5/9);
+  return Math.round(celcius * 10) / 10;
 };
 
-const ctof = function() {
-
+const ctof = function(ctemp) {
+  let farenheit = (ctemp * (9/5)) + 32;
+  return Math.round(farenheit * 10) / 10;
 };
 
 // Do not edit below this line
