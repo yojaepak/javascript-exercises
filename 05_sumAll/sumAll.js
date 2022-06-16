d9742a7fa2b06f014240aacb3c58851c8ae1a056
Young Park <yjp.youngpark@gmail.com>
2022-06-16 17:50:48 -0400

    Check for non-numbers and negatives. Input no longer order specific.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function(start, end) {
+const sumAll = function(num1, num2) {
     let result = 0;
+    let start;
+    let end;
+
+    if (num1 > num2) {
+        start = num2;
+        end = num1;
+    }
+    else {
+        start = num1;
+        end = num2;
+    }
+
+
+    if (num1 < 0 || num2 < 0) {
+        return "ERROR";
+    }
+    else if (typeof num1 !== 'number' || typeof num2 !== 'number') {
+        return "ERROR";
+    }
+
     for (let i = start; i < end + 1; i++) {
         result += i;
     }
