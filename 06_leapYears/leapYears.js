28914f28da6214333bb65dd7ca5ee672a731f292
Young Park <yjp.youngpark@gmail.com>
2022-06-16 17:57:38 -0400

    identify if input is leap year

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,17 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    
+    if (year % 4 === 0 && year % 100 === 0) {
+        return true;
+    }
+    else if (year % 4 === 0 && year % 100 !== 0) {
+        return true;
+    }
+    else if (year % 4 !== 0 && year % 100 === 0) {
+        return false
+    }
+    else {
+        return false;
+    }
 
 };
 
