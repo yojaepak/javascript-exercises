8facdaafd7a730d898570660ff648cd5d09f07ae
Young Park <yjp.youngpark@gmail.com>
2022-06-16 18:08:37 -0400

    account for special case leap years

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,13 +1,10 @@
 const leapYears = function(year) {
     
-    if (year % 4 === 0 && year % 100 === 0) {
+    if (year % 400 === 0) {
         return true;
     }
     else if (year % 4 === 0 && year % 100 !== 0) {
         return true;
-    }
-    else if (year % 4 !== 0 && year % 100 === 0) {
-        return false
     }
     else {
         return false;
