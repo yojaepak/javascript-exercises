fd0bf5f2b247790abca705ef26d7187958451dd9
Young Park <yjp.youngpark@gmail.com>
2022-07-25 12:01:08 -0400

    Fibonacci for positive whole numbers
    will loop and add current and last values until the end determined
    by input. Loop starts at 2 because first 2 numbers (1,1) are included
    at the start.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(input) {
+    let value = 0;
+    let last = 1;
+    let current = 1;
+    while (input > 2) {
+        value = last + current;
+        last = current;
+        current = value;
+        input--;
+    }
+    
+    return value;
 
 };
 
