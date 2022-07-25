9c4ae77040b4e9058003ac53f3a4c63132430fde
Young Park <yjp.youngpark@gmail.com>
2022-07-25 11:36:01 -0400

    account for uppercase

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,8 +1,10 @@
 const palindromes = function (input) {
     let original = input.replace(/[^\w\s\']|_/g, "").replace(/\s+/g, " ");
+    original = original.toLowerCase();
     let splitting = original.split(""); 
     let reverse = splitting.reverse();
     let reversed = reverse.join("");
+    reversed.toLowerCase();
     if (original === reversed) {
         return true;
     }
