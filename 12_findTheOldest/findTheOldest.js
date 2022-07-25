d9c89516663758e919ca87e66a4293a43f7bb981
Young Park <yjp.youngpark@gmail.com>
2022-07-25 13:00:37 -0400

    Find oldest in group
    Will sort people from oldest to youngest and then return the oldest
    person's name. Unde the case that a person is still living, the current
    date is used to find their age.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,27 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(input) {
+    const oldest = input.sort(function(a,b) {
+        if (a.yearOfDeath === undefined) {
+            let today = new Date();
+            let year = today.getFullYear();
+            const firstPerson = year - a.yearOfBirth;
+            const nextPerson = b.yearOfDeath - b.yearOfBirth;
+            return firstPerson > nextPerson ? -1 : 1;
+        }
+        else if (b.yearOfDeath === undefined) {
+            let today = new Date();
+            let year = today.getFullYear();
+            const nextPerson = year - b.yearOfBirth;
+            const firstPerson = a.yearOfDeath - a.yearOfBirth;
+            return firstPerson > nextPerson ? -1 : 1;
+        }
+        else {
+            const firstPerson = a.yearOfDeath - a.yearOfBirth;
+            const nextPerson = b.yearOfDeath - b.yearOfBirth;
+            return firstPerson > nextPerson ? -1 : 1;
+        }
+        
+    });
+    return oldest[0];
 };
 
 // Do not edit below this line
